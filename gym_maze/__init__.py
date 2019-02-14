8977774cc7b9e87e946e916a41b8f1e2cb02f1bc
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-02-14 15:39:58 +0000

    Add env timestep limit

--- gym_maze/__init__.py
+++ gym_maze/__init__.py
@@ -4,4 +4,11 @@ from gym_maze.env import MazeEnv
 register(
     id='Maze-v0',
     entry_point='gym_maze:MazeEnv',
+    max_episode_steps = 100,
+)
+
+register(
+    id='GoalMaze-v0',
+    entry_point='gym_maze:GoalMazeEnv',
+    max_episode_steps = 200,
 )
